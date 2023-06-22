3422ca1f84d05b614c99657d062df63ce2340593
DreamWalker06 <kiru59@hotmail.fr>
2023-06-22 16:47:08 +0200

    First commit

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Error404 from "./pages/Error404";
+import "./assets/styles/app.scss";
+
+const App = () => {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="*" element={<Error404 />} />
+      </Routes>
+    </BrowserRouter>
+  );
+};
+
+export default App;
